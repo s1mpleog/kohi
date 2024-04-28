9309b5af88673f8a2f76f39cca4ecc818cb41537
= <=>
2024-04-28 11:35:51 +0530

    Application Layer and Entry Point (1/2)

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -5,20 +5,5 @@
 #include <platform/platform.h>
 
 int main(void) {
-    KFATAL("A test Message: %f", 3.14f);
-    KERROR("A test Message: %f", 3.14f);
-    KWARN("A test Message: %f", 3.14f);
-    KINFO("A test Message: %f", 3.14f);
-    KDEBUG("A test Message: %f", 3.14f);
-    KTRACE("A test Message: %f", 3.14f);
-
-    platform_state state;
-    if (platform_startup(&state, "Kohi Engine Testbed", 100, 100, 1200, 720)) {
-        while (TRUE) {
-            platform_pump_messages(&state);
-        }
-    }
-    platform_shutdown(&state);
-
-    return 0;
+      return 0;
 }
